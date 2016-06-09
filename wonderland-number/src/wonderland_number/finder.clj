0e879160de824ebacdeca265d0dfdb3eb372295f
Jeffrey Simon <jdsimon@gmail.com>
2016-06-09 14:36:57 -0400

    better readability\?

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -5,10 +5,10 @@
     (= (f i) (f j))))
 
 (defn wonderland-number-candidates []
-  (let [two-six (range 2 (inc 6))]
-    (for [n (range 100000 1000000)]
-      (let [mults-same-digits (map #(same-digits? n (* n %)) two-six)]
-        {:n n :same-digits? (every? true? mults-same-digits)}))))
+  (for [n (range 100000 1000000)]
+    (let [mults (map (partial * n) (range 2 (inc 6)))
+          mults-same-digits (map (partial same-digits? n) mults)]
+      {:n n :same-digits? (every? true? mults-same-digits)})))
 
 (defn wonderland-number []
   (->> (wonderland-number-candidates)
