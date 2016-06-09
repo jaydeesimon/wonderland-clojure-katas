c86630b5153d005cfd3705a8b89913415e6a8880
Jeffrey Simon <jdsimon@gmail.com>
2016-06-08 21:59:26 -0400

    wonderland number

--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,14 @@
 (ns wonderland-number.finder)
 
+(defn same-digits? [i j]
+  (let [f (comp frequencies str)]
+    (= (f i) (f j))))
+
+(defn wonderland-number-candidates []
+  (for [n (range 100000 1000000)]
+    (let [two-six (range 2 (inc 6))
+          sd? (map #(same-digits? n (* n %)) two-six)]
+      {:n n :same-digits? (every? true? sd?)})))
+
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (:n (first (filter :same-digits? (wonderland-number-candidates)))))
